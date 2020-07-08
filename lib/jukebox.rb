66c3b91d4c9f6ce6dc8eb9236f6c0ef37e0a9375
Brian Lego <legobw@gmail.com>
2020-07-08 15:07:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,11 +1,9 @@
 def help
-  
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
   puts "- play : lets you choose a ong to play"
   puts "- exit : exits this program"
-  
 end
 
 
