bfa60bd29ae2340a1ca8a8bbf1fe76982177f881
Brian Lego <legobw@gmail.com>
2020-07-08 18:17:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,7 +32,7 @@ def play(songs)
   user_input = gets.strip
   binding.pry
   if user_input.to_i <= songs.length
-    puts "Playing #{songs[user_input.to_i]}"
+    puts "Playing #{songs[user_input.to_i-1]}"
     binding.pry
   end
 end  
