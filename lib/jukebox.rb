8837bf3a7ade72d9a1f91eb557a695892d02c8bb
Brian Lego <legobw@gmail.com>
2020-07-08 15:01:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,15 @@
-# Add your code here
+def help
+  
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a ong to play"
+  puts "- exit : exits this program"
+  
+end
+
+
+def list (songs_array)
+  
+  
+end  
