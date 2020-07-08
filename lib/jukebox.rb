71fcefc51be96597868f6578c592b1a511f17284
Brian Lego <legobw@gmail.com>
2020-07-08 18:26:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
   #binding.pry
-  if user_input.to_i == (1..9)
+  if user_input.to_i = (1..9)
     #binding.pry
     puts "Playing #{songs[user_input.to_i-1]}"
   #elsif songs.each {|song| user_input == song}
