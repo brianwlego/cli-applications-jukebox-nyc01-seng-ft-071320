740ecb4979380139e8ed4723526b88d781284de0
Brian Lego <legobw@gmail.com>
2020-07-08 15:57:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,28 +30,16 @@ end
 def play (songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
+    
     case user_input
-      when "1" || "1901"
-        puts "Playing Phoenix - 1901"
-      when "2" || "Wait Up"
-        puts "Playin Tokyo Police Club - Wait Up"
-      when "3" || "Too Much"
-        puts "Playing Sufjan Stevens - Too Much"
-      when "4" || "Young Blood"
-        puts "Playing The Naked and the Famous - Young Blood"
-      when "5" || "Tiga"
-        puts "Playing (Far From) Home - Tiga"
-      when "6" || "Abducted"
-        puts "Playing The Cults - Abducted"
-      when "7" || "Consolation Prizes"
-        puts "Playing Phoenix - Consolation Prizes"
-      when "8" || "Cats in the Cradle"
-        puts "Playing Harry Chapin - Cats in the Cradle"
-      when "9" || "Keep It Loose, Keep It Tight"
-        puts "Playing Amos Lee - Keep It Loose, Keep It Tight"
+      songs.each do |song|
+      binding.pry
+      when 
+        
       else
         puts "Invalid input, please try again"
-        play(songs)
+        #play(songs)
+      end  
     end
 end  
 
