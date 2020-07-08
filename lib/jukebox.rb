df7262b8e095992737803fbc47593f81c64fc0b0
Brian Lego <legobw@gmail.com>
2020-07-08 15:58:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,17 +30,16 @@ end
 def play (songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
-    
+  songs.each do |song|
+    binding.pry
     case user_input
-      songs.each do |song|
-      binding.pry
       when 
         
       else
         puts "Invalid input, please try again"
         #play(songs)
-      end  
     end
+  end  
 end  
 
 def exit_jukebox 
