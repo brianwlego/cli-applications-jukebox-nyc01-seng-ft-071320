a99c3fdb3142da8fdfdae117db607a78c9412c70
Brian Lego <legobw@gmail.com>
2020-07-08 16:43:07 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,7 +36,7 @@ def play(songs)
         puts "Playing #{song}"
       when user_input == song
         puts "Playing #{song}"
-        binding.pry
+        #binding.pry
       when user_input != "#{index+1}" || song 
         puts "Invalid input, please try again"
         #binding.pry
