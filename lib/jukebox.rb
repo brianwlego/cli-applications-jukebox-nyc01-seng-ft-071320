c1faa82ee7ca5a1370c42cecd3bc6519a346f9ff
Brian Lego <legobw@gmail.com>
2020-07-08 16:02:21 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,7 +31,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
   songs.each_with_index do |song, index|
-    binding.pry
+    #binding.pry
     case user_input
       when index+1 || song
         puts "Playing #{song}"
