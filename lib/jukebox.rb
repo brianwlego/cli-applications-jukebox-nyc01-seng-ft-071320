44f884a41eb62885fcd29fb5d45e75e20d9cf9ed
Brian Lego <legobw@gmail.com>
2020-07-08 17:57:51 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -36,6 +36,7 @@ def play(songs)
     case user_input
       when song then puts "Playing #{song}"
       when "#{index+1}" then puts "Playing #{song}"
+      else puts "Invalid input, please try again"
       #binding.pry  
     end 
   end  
