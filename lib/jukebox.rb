31ef45e0200abe2bf5025013bd7de0382ad0eb90
Brian Lego <legobw@gmail.com>
2020-07-08 15:53:58 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -30,6 +30,7 @@ end
 def play (songs)
   puts "Please enter a song name or number:"
   user_input = gets.strip
+    binding.pry
     case user_input
       when "1" || "1901"
         puts "Playing Phoenix - 1901"
